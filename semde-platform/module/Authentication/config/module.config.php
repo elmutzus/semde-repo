8c9aa77a23c67e21c9e280ca701402a19d47ea99
elmutzus <elmutzus@gmail.com>
2017-04-22 13:35:31 -0600

    Changed file headers for licensing

--- semde-platform/module/Authentication/config/module.config.php
+++ semde-platform/module/Authentication/config/module.config.php
@@ -1,10 +1,19 @@
 <?php
 
+/* 
+ * Copyright (c) 2017 Elder Mutzus <elmutzus@inspireswgt.com>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Elder Mutzus <elder.mutzus@inspireswgt.com> - initial API and implementation and/or initial documentation
+ */
+
 namespace Authentication;
 
 use Zend\Router\Http\Literal;
-use Zend\Router\Http\Segment;
-use Zend\ServiceManager\Factory\InvokableFactory;
 use Doctrine\DBAL\Driver\PDOMySql\Driver as PDOMySqlDriver;
 use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
 
--- semde-platform/module/Authentication/src/Module.php
+++ semde-platform/module/Authentication/src/Module.php
@@ -1,9 +1,14 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * Copyright (c) 2017 Elder Mutzus <elmutzus@inspireswgt.com>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Elder Mutzus <elder.mutzus@inspireswgt.com> - initial API and implementation and/or initial documentation
  */
 
 namespace Authentication;
