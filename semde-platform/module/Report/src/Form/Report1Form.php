65beafa9ddef6782716d817f8881fdba1eeb7462
elmutzus <elmutzus@gmail.com>
2017-07-29 22:30:42 -0600

    Minor fixes in text

--- semde-platform/module/Report/src/Form/Report1Form.php
+++ semde-platform/module/Report/src/Form/Report1Form.php
@@ -106,7 +106,7 @@ class Report1Form extends Form
                 'class' => 'form-control',
             ],
             'options'    => [
-                'label'         => 'Filtrar por',
+                'label'         => 'Semestre',
                 'value_options' => [
                     '-1' => 'Todos',
                     '1' => 'Primer semestre',
